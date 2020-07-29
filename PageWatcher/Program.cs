fa8884ce2354ee6f8c25e0d93239a7b3a9bbc838
Jack Gribben <jack.gribben@cbg.co.nz>
2020-07-29 20:09:18 +1200

    Add project files.

---
+++ PageWatcher/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace PageWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSystemWatcher fileWatcher = new FileSystemWatcher();
+            fileWatcher.Path = @"C:\nzhs\questioninformation\QuestionLogTemp\";
+            fileWatcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                        | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            fileWatcher.Filter = "*.txt";
+            fileWatcher.EnableRaisingEvents = true;
+            fileWatcher.Created += FileWatcher_Changed;
+            fileWatcher.Changed += FileWatcher_Changed;
+
+            while (true)
+            {
+                Thread.Sleep(10000);
+            }
+            
+        }
+
+        private static void FileWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            
+            string fileName = e.Name;
+            string timeStamp = DateTime.Now.ToString();
+            Console.WriteLine("file modified/created " + fileName + " " + timeStamp);
+        }
+
+
+    }
+}
